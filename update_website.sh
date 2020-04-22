955700181076b6fa9aec102e602b4b4ed3eac56e
Ivan Jayapurna <ivanfj@berkeley.edu>
2020-04-22 16:13:18 -0700

    committing to git via shell script!

---
+++ update_website.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo “updating website…”
+git add .
+git commit -m "committing to git via shell script!"
+git push origin master
+ssh ivanfj@kepler.berkeley.edu
+cd /var/www/html/htdocs-groups/xugroup/xu_group_website
+echo “pulling changes from github…”
+git pull origin master
+cp -r * ../
+echo “done!”
